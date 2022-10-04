6d9f872487262f2ad6d1a53893d0c9ede5b133bf
Shazia Khan <90377354+shaziya5416@users.noreply.github.com>
2022-10-04 10:48:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,162 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css">
-    <title>TODO App</title>
-  </head>
-  <body>
-    <main>
-      <header>
-        <h1>todos</h1>
-      </header>
-      <section class="container">
-        <input
-           type="text"
-           placeholder="What needs to be done ?"
-        >
-        <ul>
-          <!-- <li>
-            <input type="checkbox" name="checkbox" id="checkbox">
-            <span class="name"></span>
-            <button class="cross-btn">X</button>
-          </li> -->
-        </ul>
-      </section>
-      <section class="container relative">
-        <div class="outer">
-          <div class="middle">
-            <div class="edit_todo">
-              <button class="left_items btn"> <small class="left_items_data">3</small> items-left</button>
-              <button class="all_items btn">All</button>
-              <button class="active_items btn">Active</button>
-              <button class="completed_items btn">Completed</button>
-              <button class="clear_completed btn">clear completed</button>
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-    <script src="./script.js"></script>
-  </body>
-</html>
+
+let input = document.querySelector("input[type='text']");
+
+let root = document.querySelector("ul");
+
+let all =document.querySelector(".all_items");
+let active =document.querySelector(".active_items");
+let completed = document.querySelector(".completed_items") 
+let clear = document.querySelector(".clear_completed");
+
+//for storing in local storage
+let todos = localStorage.getItem('todos')
+ ? JSON.parse(localStorage.getItem('todos'))
+ : [] ;
+
+function addToDO(event){
+    if(event.keyCode === 13 && event.target.value){
+          todos.push({
+              name: event.target.value,
+              isDone: false
+          })
+          event.target.value=""
+          createUI(todos, root);
+          getNumberOfCompletedTodo();
+          localStorage.setItem("todos", JSON.stringify(todos))
+    }
+}
+input.addEventListener('keyup',addToDO)
+
+function deleteTodo(event){
+    todos.splice(event.target.dataset.id,1);
+    createUI(todos, root);
+    getNumberOfCompletedTodo();
+    toggleClearButton();
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function handleChange(event){
+    let id = event.target.dataset.id
+    todos[id].isDone = !todos[id].isDone;
+    createUI(todos, root);
+    getNumberOfCompletedTodo();
+    toggleClearButton();
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+
+// creating whole UI
+function createUI(allTodo = todos, rootElement){
+    root.innerHTML = ""
+
+   allTodo.forEach( (todo, index) => {
+    let li = document.createElement("li");
+    li.classList.add('li')
+
+    let checkbox = document.createElement('input')
+    checkbox.setAttribute('type','checkbox')
+    checkbox.setAttribute('data-id',index)
+    checkbox.checked = todo.isDone;
+    checkbox.addEventListener('change', handleChange)
+
+    let span = document.createElement('span')
+    span.innerText = todo.name;
+    span.classList.add('todo-name') 
+
+    let button = document.createElement('button')
+    button.innerText ='X';
+    button.classList.add('cross-btn');
+    button.setAttribute("data-id",index)
+    button.addEventListener('click',deleteTodo)
+
+    li.append(checkbox,span,button)
+
+    rootElement.append(li) 
+   })
+}
+
+// left items 
+function getNumberOfCompletedTodo(){
+    document.querySelector(".left_items_data")
+    .innerText = todos.reduce( (acc, todo)=>{
+        if(!todo.isDone){
+            return ++acc;
+        }
+        return acc;
+    },0)
+}
+
+// all items 
+all.addEventListener('click',function(){
+    createUI(todos, root);
+    updateActiveButton("all");
+})
+
+// creating active items UI 
+
+active.addEventListener('click',function(){
+    let tempArray= todos.filter( (todo) => {
+        return !todo.isDone;
+      })
+      createUI(tempArray, root);
+      updateActiveButton("active");
+})
+
+// creating completed todos UI
+
+completed.addEventListener('click',function(){
+    let tempArray= todos.filter( (todo) => {
+        return todo.isDone;
+      })
+      createUI(tempArray, root);
+      updateActiveButton("completed")
+})
+
+// clearing completed
+
+clear.addEventListener('click',function(){
+    todos = todos.filter((todo)=>{
+        return !todo.isDone;
+    })// assigning todos a new uncompleted list of todos
+    createUI(todos, root)
+    localStorage.setItem("todos", JSON.stringify(todos)) 
+})
+
+// let current
+// function donotSwitch(){
+
+// }
+
+function toggleClearButton(){
+   let isTrue = todos.some(todo =>{
+        return todo.isDone;
+    })
+    if(isTrue){
+        clear.classList.add('show')
+    }else{
+        clear.classList.remove('show')
+        clear.classList.add('hide')
+    }
+
+}
+toggleClearButton();
+
+function updateActiveButton(btn){
+    all.classList.remove('selected');
+    active.classList.remove('selected')
+    completed.classList.remove('selected')
+    if(btn === "all"){
+        all.classList.add('selected')
+    }
+    if(btn ==="active"){
+        active.classList.add('selected')
+    }
+    if(btn === "completed"){
+        completed.classList.add('selected')
+    }
+}
+
+
+all.classList.add('selected')
+createUI(todos, root);
+getNumberOfCompletedTodo();
